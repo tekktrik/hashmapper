89a1855e8041c63a0c277a3192efdd150ebf0962
Alec Delaney <tekktrik@gmail.com>
2025-11-20 23:53:16 -0500

    Fix map bounds

--- src/components/UploadMap.tsx
+++ src/components/UploadMap.tsx
@@ -44,9 +44,11 @@ export const UploadMap: React.FC = () => {
         filename={filename}
       ></GPXFileUpload>
       {/* This API key can eventually be public if it's online, once access is restricted */}
-      <APIProvider apiKey={import.meta.env.VITE_GMAPS_API_KEY}>
-        <StopsMap points={points} route={route}></StopsMap>
-      </APIProvider>
+      <div style={{ width: "80vw", height:"75vh", marginLeft: "calc(50% - 40vw)" }}>
+        <APIProvider apiKey={import.meta.env.VITE_GMAPS_API_KEY}>
+          <StopsMap points={points} route={route}></StopsMap>
+        </APIProvider>
+      </div>
     </>
   );
 };
