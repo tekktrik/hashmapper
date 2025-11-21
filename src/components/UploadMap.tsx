e252c90bcac9b31ef974e8c7728ae3d107baf411
Alec Delaney <tekktrik@gmail.com>
2025-11-21 00:10:43 -0500

    Change height of map

--- src/components/UploadMap.tsx
+++ src/components/UploadMap.tsx
@@ -44,7 +44,7 @@ export const UploadMap: React.FC = () => {
         filename={filename}
       ></GPXFileUpload>
       {/* This API key can eventually be public if it's online, once access is restricted */}
-      <div style={{ width: "80vw", height:"75vh", marginLeft: "calc(50% - 40vw)" }}>
+      <div style={{ width: "80vw", height:"70vh", marginLeft: "calc(50% - 40vw)" }}>
         <APIProvider apiKey={import.meta.env.VITE_GMAPS_API_KEY}>
           <StopsMap points={points} route={route} />
         </APIProvider>
