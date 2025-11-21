55727b3ae4324f0f6bbc6255f4ee31b3ab1a8617
Alec Delaney <tekktrik@gmail.com>
2025-11-21 00:18:00 -0500

    Fix app name

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import "bulma/css/bulma.min.css";
 import "./App.css";
 import { UploadMap } from "./components/UploadMap";
 
-const APP_NAME = "Hash Mapper";
-
 function App() {
   return (
     <>
       <div className="App">
-        <title>{APP_NAME}</title>
-        <h1 className="title is-2">{APP_NAME}</h1>
+        <h1 className="title is-2">Hash Mapper</h1>
         <UploadMap />
       </div>
     </>
